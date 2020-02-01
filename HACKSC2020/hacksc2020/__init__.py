75d1320ec36a1f38ac9ad4de50936e4ba377f472
Iyasu Gessesse <igessess@uci.edu>
2020-02-01 05:18:05 -0800

    init

---
+++ HACKSC2020/hacksc2020/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '3a32e715d3f785958807d703a535a70d'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+# db = SQLAlchemy(app)
+
+
+from hacksc2020 import routes # make routes public
+from hacksc2020 import forms
---
+++ HACKSC2020/hacksc2020/routes.py
@@ -0,0 +1,20 @@
+from flask import Flask, escape, request,render_template,url_for,flash,redirect
+from hacksc2020 import app#,db
+from hacksc2020.forms import RegistrationForm, LoginForm
+
+@app.route('/',methods =["GET","POST"])
+def home():
+    return render_template("home.html",title="Home")
+
+@app.route('/register',methods =["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        return redirect(url_for("home"))
+    return render_template("register.html",title="Register",form=form)
+
+
+@app.route('/login',methods =["GET","POST"])
+def login():
+    form = RegistrationForm()  
+    return render_template("login.html",title="Login",form=form)
