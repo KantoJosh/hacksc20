007ead42ba964565bdb430c09186e91b1f8475db
Iyasu Gessesse <igessess@uci.edu>
2020-02-02 00:20:33 -0800

    menu->cart

--- HACKSC2020/run.py
+++ HACKSC2020/run.py
@@ -6,13 +6,10 @@ if __name__ == "__main__":
     #db.session.commit()
 
     from hacksc2020.models import User,Item
-    item1 = Item(image="boo.jpg",name="name",price=4.20)
-    item2 = Item(image="boo2.jpg",name="name2",price=2.40)
 
     __USER__ = User(username = "USER",email="user@hacksc.com",password="hacksc")
     db.session.add(__USER__)
     db.session.commit()
-    print(User.query.all())
-    print(Item.query.all())
+    print("USERS: ",User.query.all())
 
     app.run(debug = True)
